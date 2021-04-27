e2cb9ad35fd5fcabfed29570f2da503b3707c607
Abhilash Kumar <v-abku13@microsoft.com>
2021-04-27 16:29:15 +0530

    First a.py commit

---
+++ a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import pyautogui 
+
+def main():
+	i=0
+	x=100
+	y=100
+	while (i<=10000000000000000000000000):
+		pyautogui.moveTo(x, y, duration = 10)
+		pyautogui.click(x, y) 
+		pyautogui.moveTo(x-10, y-10, duration = 10)
+		pyautogui.click(x-10, y-10) 
+		i=i+1
+
+if __name__ == "__main__": main()
